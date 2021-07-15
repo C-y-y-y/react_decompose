7c9399cff8ba404d940281b1361bfa3bc0d1d855
Yaroslav <gso218695@gmail.com>
2021-07-15 21:42:51 +0300

    Updates

---
+++ precache-manifest.e9ad8cbaabed588ee184e7a334623c6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3869e8c378e2cad1d3cbaf2013d30fdb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9d11d24ca160b9533da8",
+    "url": "./static/css/main.e9dcec3e.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d11d24ca160b9533da8",
+    "url": "./static/js/main.29dd2f76.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
